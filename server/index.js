9747af6275d72bed1c9bf1e2db79f984f4e20b6b
vipulchandan <plenteousvipul90009@gmail.com>
2023-08-27 07:57:08 +0530

    Pictopia

---
+++ server/index.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+
+import connectDB from './mongodb/connect.js';
+import postRoutes from './routes/postRoutes.js';
+import dalleRoutes from './routes/dalleRoutes.js';
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json({ limit: '50mb' }));
+
+app.use('/api/v1/post', postRoutes);
+app.use('/api/v1/dalle', dalleRoutes);
+
+app.get('/', async (req, res) => {
+    res.send('Hello from DALL-E!');
+});
+
+const PORT = process.env.PORT || 8080;
+
+const startServer = async () => {
+    try {
+        connectDB(process.env.MONGO_URI);
+        app.listen(PORT, () => {
+            console.log(`Server listening on port ${PORT}`);
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+startServer();
